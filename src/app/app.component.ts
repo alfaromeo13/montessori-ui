00b4aaef12935b3cb10fc0ea6dcf86a8bc21bee9
Andjela Vucic <andjelaavucic@gmail.com>
2025-01-04 22:51:13 +0100

    fix: logout admin on router changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'montessori-ui';
+  private adminPaths: string[] = [ '/admin', '/admin/panel', '/admin/edit' ];
+
+  constructor(private router: Router,
+              private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      const url: string = (event as NavigationEnd).url;
+      const isAdminRoute: boolean = this.adminPaths.some((path: string) => url.startsWith(path));
+      if (!isAdminRoute && this.authService.getAuthStatus()) {
+        this.authService.logout();
+      }
+    });
+  }
 }
